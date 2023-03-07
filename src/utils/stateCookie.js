13be2f670a1cfb03149d151cc52326b808438ece
Theagen <CTheagen@uwhealth.org>
2023-03-07 16:57:53 -0600

    docs: jsdocs for `utils/stateCookie` fns; main readme brought up-to-date

--- src/utils/stateCookie.js
+++ src/utils/stateCookie.js
@@ -1,18 +1,59 @@
 import Cookies from 'js-cookie';
 
 
+/**
+ * Network cookie name/key.
+ * Used in calculation of initial state value.
+ */
 export const SERVER_STATE_COOKIE_KEY = 'uwh_state';
+
+/**
+ * Local cookie name/key.
+ * Used in calculation of initial state value
+ * and updated on user toggle.
+ */
 export const LOCAL_STATE_COOKIE_KEY = `${SERVER_STATE_COOKIE_KEY}-local`;
 
+/**
+ * Default Wisconsin cookies' value.
+ */
 export const STATE_COOKIE_VALUE_WISCONSIN = 'wi';
+
+/**
+ * Default Illinois cookies' value.
+ */
 export const STATE_COOKIE_VALUE_ILLINOIS = 'il';
+
+/**
+ * Array of default states' cookies' values.
+ */
 export const STATE_COOKIE_VALUES = [STATE_COOKIE_VALUE_WISCONSIN, STATE_COOKIE_VALUE_ILLINOIS];
 
+/**
+ * Validates cookie value.
+ */
+
+/**
+ * Returns current local state cookie value.
+ */
 export const getLocalStateCookie = () => Cookies.get(LOCAL_STATE_COOKIE_KEY);
+
+/**
+ * Returns current network state cookie value.
+ */
 export const getServerStateCookie = () => Cookies.get(SERVER_STATE_COOKIE_KEY);
-// local > server
+
+/**
+ * Returns initial state cookie value.
+ * (local > server)
+ */
 export const getInitialStateCookie = () => getLocalStateCookie() || getServerStateCookie();
 
+/**
+ * Maps state cookie values to "pretty" state values.
+ * @param {string} stateCookieValue State's cookie value.
+ * @return {string} "pretty" state value.
+ */
 export const getStateCookiePrettyValue = (stateCookieValue) => {
   switch (stateCookieValue) {
     case 'il':
@@ -22,9 +63,15 @@ export const getStateCookiePrettyValue = (stateCookieValue) => {
   };
 };
 
-export const setLocalStateCookie = (value) => Cookies.set(
+/**
+ * Updates the local state cookie,
+ * keeping it in-sync with Svelte store.
+ * @param {string} storeValue Value from store to update cookie with.
+ * @return Updated local state cookie.
+ */
+export const setLocalStateCookie = (storeValue) => Cookies.set(
   LOCAL_STATE_COOKIE_KEY,
-  value,
+  storeValue,
   {
     expires: 30,
     secure: true,
